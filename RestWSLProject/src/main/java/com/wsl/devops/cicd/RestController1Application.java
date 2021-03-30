c31ad480be11711f2af4f8359f1b8c7a313dd2d9
giteauser <gitea@fake.local>
2021-03-30 13:43:36 +0530

    First Commit

---
+++ RestWSLProject/src/main/java/com/wsl/devops/cicd/RestController1Application.java
@@ -0,0 +1,13 @@
+package com.wsl.devops.cicd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestController1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestController1Application.class, args);
+	}
+
+}
---
+++ RestWSLProject/src/test/java/com/wsl/devops/cicd/RestController1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wsl.devops.cicd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestController1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
